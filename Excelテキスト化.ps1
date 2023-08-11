7d3b6b0817fda48f73da554a897591054d3b23d9
negitoro-gohan <44143453+negitoro-gohan@users.noreply.github.com>
2023-08-12 00:01:22 +0900

    Add files via upload

---
+++ Excelテキスト化.ps1
@@ -0,0 +1,78 @@
+﻿function outputLog($logString){
+
+  $Now = Get-Date
+
+  # Log 出力文字列に時刻を付加(YYYY/MM/DD HH:MM:SS.MMM $logString)
+  $Log = $Now.ToString("yyyy/MM/dd HH:mm:ss.fff") + " "
+  $Log += $logString
+
+  # echo させるために出力したログを戻す
+  Return $Log
+}
+<#
+ExcelToTSV
+概要
+ エクセルをTSVに変換する。※1…エクセルの1つめのシートを対象にする。※2…[temp]というシートを一時シートとするので、エクセルの[temp]シートが存在しないことを前提とする。
+引数
+ targetFile:エクセルのパス,
+ outFile:出力するファイル名,
+ startRow:TSV化を開始する行番号,
+ startCol:TSV化を開始する列番号,
+ existsCol:データが記載されているとみなす列番号(TSVに変換する際この列番号に記載されてる値の最大の行番号までをTSVに変換するため、この引数を指定する)
+ existsRow:データが記載されているとみなす行番号(TSVに変換する際この行番号に記載されてる値の最大の列番号までをTSVに変換するため、この引数を指定する)
+#>
+function ExcelToTSV($targetFile, $outFile, $startRow, $startCol, $existsCol, $existsRow) {
+  
+  # ファイル名の出力
+  $OutFile = (Split-Path -Parent $targetFile)+ '\'+ $OutFile
+  outputLog ("[" + (Split-Path $targetFile -Leaf) + "]をテキスト化します。")
+
+  # Excelファイルの存在チェック
+  if (!(Test-Path $targetFile)) {
+    outputLog "ファイルが存在しません。処理を中止します。"
+    exit
+  }
+  
+  #対象のEXCELを開き、一時シート追加
+  $xl = New-Object -ComObject Excel.Application
+  $wb = $xl.Workbooks.Open($targetFile)
+  $targetsh = $wb.Worksheets.Add([System.Reflection.Missing]::Value, $wb.Sheets($wb.Sheets.Count))
+  $targetsh.name = "temp"
+  $sourceSheet = $wb.Worksheets.Item(1) 
+  
+  # コピー範囲の特定
+  #最大行数の特定
+  $lastRow = $sourceSheet.Range($sourceSheet.Cells(1000000, $existsCol), $sourceSheet.Cells(1000000, $existsCol)).End([Microsoft.Office.Interop.Excel.XlDirection]::xlUp.value__).Row
+  outputLog ('最大行数は' + $lastRow + 'です')
+  
+  #最大列数の特定
+  $lastCol = $sourceSheet.Range($sourceSheet.Cells($existsRow, 10000), $sourceSheet.Cells($existsRow, 10000)).End([Microsoft.Office.Interop.Excel.XlDirection]::xlToLeft.value__).Column
+  outputLog ('最大列数は' + $lastCol + 'です')
+  
+  #出力対象件数の出力
+  outputLog ('出力対象データは' + ($lastrow - ($startRow - 1)) + '件です')
+  
+  # コピー範囲をクリップボードにコピーしてファイル出力
+  $SourceRange = $sourceSheet.range($sourceSheet.Cells($startRow, $startCol), $sourceSheet.Cells($lastrow, $lastCol))
+  if (!($SourceRange.copy())) {
+    outputLog "コピーが失敗しました。"
+    $wb.Close()
+    $xl.Quit()
+    exit
+  }
+  $Range = $targetsh.Range("A1")
+  $targetsh.paste($Range) # 貼り付け
+  $targetsh.Activate()
+  $xl.DisplayAlerts = $FALSE
+  $wb.SaveAs($OutFile , [Microsoft.Office.Interop.Excel.XlFileFormat]::xlUnicodeText)  #ファイル出力
+  $wb.Close()
+  $xl.Quit()
+
+  #デフォルトの文字コードでファイル出力(Windowsの場合デフォルトの文字コードはShift-JIS)
+  $data=Get-Content ($OutFile)
+  $data | Out-File ($OutFile) -Encoding default 
+  outputLog ("[" + (Split-Path $targetFile -Leaf) + "]のテキスト化が完了しました。")
+
+
+}
+
---
+++ 指定したテーブルのInsert文を作成する.ps1
@@ -0,0 +1,35 @@
+﻿[void][Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.Smo")
+
+#設定情報
+$serverName = 'インスタンス名'
+$databaseName = "データベース名"
+$username = "ユーザ"
+$password = "パスワード"
+$scriptDir = "フォルダ名"
+$targetTbl = "テーブル名"
+
+#スクリプト作成のオプション設定
+$scripter = new-object Microsoft.SqlServer.Management.Smo.Scripter $serverName
+$scripter.Options.ScriptSchema = $false; 
+$scripter.Options.ScriptData = $true; 
+$scripter.Options.NoCommandTerminator = $true; 
+$scripter.Options.ToFileOnly = $true 
+
+#接続確立
+$server = new-object Microsoft.SqlServer.Management.Smo.Server $serverName
+$server.ConnectionContext.LoginSecure = $false
+$server.ConnectionContext.Login = $username
+$server.ConnectionContext.password = $password
+
+#出力選択
+$db  = $server.Databases[$databaseName]
+$tbl = $db.Tables | Where-object {$_.name -eq $targetTbl} 
+
+#ファイル出力
+foreach ($s in $tbl) { 
+    $tableName = $s.Name
+    #write-host $s "$scriptDir\$tableName.sql"
+    $scripter.Options.FileName = "$scriptDir\$tableName.sql"          #出力先ファイル
+    $scripter.EnumScript($s.Urn)
+} 
+
